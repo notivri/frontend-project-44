cae4563c3cabd38b482b95c19169b05e71186f6b
notivri <katsu4973@gmail.com>
2025-02-24 16:50:26 +0700

    Adjustments for auto-test т-т

--- bin/games/parityGame.js
+++ bin/games/parityGame.js
@@ -8,18 +8,23 @@ const game = (name) => {
     const number = randomNumber();
 
     console.log(`[${counter}|3] Number is: ${number}`);
-    const answer = readlineSync.question('Is it even? ');
+    const answer = readlineSync.question('Your answer: ');
+
+    const correctAnswer = number % 2 === 0 ? 'yes' : 'no';
 
     if (checkParityGuess(answer.toLowerCase(), number)) {
       counter += 1;
       console.log('Correct!');
     } else {
-      counter = 0;
-      console.log(`You're wrong, ${name}! Try again`);
+      console.log(`${answer} is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${name}!`);
+      break;
     }
   }
 
-  console.log(`Congratulations, ${name}! You win`);
+  if (counter === 3) {
+    console.log(`Congratulations, ${name}!`);
+  }
 };
 
 export default game;
